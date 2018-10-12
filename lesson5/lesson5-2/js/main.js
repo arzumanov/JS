2480805387acf87a8a9dd2585e9fcab6d6c248dd
unknown <mr.arzumanoffgit config --global user.email mr.arzumanoff@yandex.rugit config --global user.email mr.arzumanoff>
2018-10-12 23:07:27 +0300

    Fifth lesson 2

--- lesson5/lesson5-2/js/main.js
+++ lesson5/lesson5-2/js/main.js
@@ -1,8 +1,38 @@
 let btn = document.getElementById('start'),
-    // value = document.querySelectorAll('.result-table');
-    expenses = document.getElementsByClassName('expenses-item')
-    btny = document.getElementsByTagName('button');
+    budget = document.getElementsByClassName('budget-value'),
+    daybudget = document.getElementsByClassName('daybudget-value'),
+    level = document.getElementsByClassName('level-value'),
+    expensesV = document.getElementsByClassName('expenses-value'),
+    optionalexpenses = document.getElementsByClassName('optionalexpenses-value'),
+    income = document.getElementsByClassName('income-value'),
+    monthsavings = document.getElementsByClassName('monthsavings-value'),
+    yearsavings = document.getElementsByClassName('yearsavings-value'),
+    expenses = document.getElementsByClassName('expenses-item'),
+    btnExpenses = document.getElementsByTagName('button')[0],
+    btnOptional = document.getElementsByTagName('button')[1],
+    btnCount = document.getElementsByTagName('button')[2],
+    optionalExpensesI = document.querySelectorAll('.optionalexpenses-item'),
+    incomeLabel = document.querySelector('.choose-income-label'),
+    savings = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    year = document.querySelector('.year'),
+    month = document.querySelector('.month'),
+    day = document.querySelector('.day');
+
 
 
 console.log(btn);
 console.log(expenses);
+console.log(btnExpenses);
+console.log(btnOptional);
+console.log(btnCount);
+console.log(optionalExpensesI);
+console.log(savings);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
+
+
